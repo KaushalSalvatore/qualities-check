7096c71d0d5d0dedad326e5b6b439e9385735308
kaushal <kaushalpandey455@gmail.com>
2023-09-08 12:44:59 +0530

    app.py

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def waterQualitiy():
 			return render_template('waterResult.html', prediction=prediction)
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+	app.run()
